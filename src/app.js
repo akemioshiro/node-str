15f1944ea87ebea62aaa4381b7273f18ded213b0
patricia.oshiro@totvs.com.br <patricia.oshiro@totvs.com.br>
2019-03-14 20:07:52 -0300

    separação dos arquivos das rotas

--- src/app.js
+++ src/app.js
@@ -4,41 +4,20 @@ const bodyParser = require('body-parser')
 const app = express();
 const router = express.Router();
 
+// Carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 // todo conteudo sera convertido para json
 app.use(bodyParser.json());
 // codifica a url
 app.use(bodyParser.urlencoded({ extended: false}));
 
-// cria uma rota na raiz
-const route = router.get('/', (req, res, next) =>{
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
 
-const create = router.post('/', (req, res, next) =>{
-    res.status(201).send(req.body);
-});
 
-const put = router.put('/:id', (req, res, next) =>{
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
-    });
-});
 
-const del = router.delete('/', (req, res, next) =>{
-    res.status(200).send({
-        item: req.body
-    });
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 // exporta a aplicaçãp
 module.exports = app;
